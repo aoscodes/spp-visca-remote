0b0f25509af1966339d855e013e367e2da84e4b2
tyler williams <tyler@aos.codes>
2025-06-25 20:15:56 -0400

    add basic express router

--- index.ts
+++ index.ts
@@ -1,29 +1,55 @@
 import { ViscaCamera, ViscaCommand } from 'visca-over-ip'
+import express, { Router } from 'express'
 
-var camera = new ViscaCamera('192.168.1.100', 1259);
-var command = ViscaCommand.cameraPresetRecall(1)
+import dotenv from 'dotenv'
+dotenv.config()
 
-command.on('ack', (data) => {
-  console.log('Command acknowledged:', data)
+const app = express()
+let camera: ViscaCamera
+
+const router = Router()
+
+router.get('/:pose', async (req, res) => {
+  const pose = Number.parseInt(req.params.pose)
+  if (!pose) res.send(404)
+
+  const command = ViscaCommand.cameraPresetRecall(pose)
+
+  const result = await cameraCommand(camera, command)
+
+  res.send(result)
 })
 
-command.on('error', (err) => {
-  console.log('Command error: ', err)
+app.listen(8888, () => {
+  camera = new ViscaCamera('192.168.1.100', 1259);
+  camera.on('connected', () => {
+    console.log('Camera connected')
+  })
+
+  camera.on('error', (err) => {
+    console.log("error ", err)
+  })
+
+  camera.on('closed', (event) => {
+    console.log("closed? ", event)
+  })
 })
 
-command.on('complete', (data) => {
-  console.log('Command complete: ', data)
-})
+const cameraCommand = async (camera: ViscaCamera, command: ViscaCommand) => {
+  command.on('ack', (data) => {
+    console.log('Command acknowledged:', data)
+    return { type: "ack", data: data }
+  })
 
-camera.on('connected', () => {
-  console.log('Camera connected')
+  command.on('error', (err) => {
+    console.log('Command error: ', err)
+    return { type: "error", data: err }
+  })
+
+  command.on('complete', (data) => {
+    console.log('Command complete: ', data)
+    return { type: "complete", data: data }
+  })
+
   camera.sendCommand(command)
-})
-
-camera.on('error', (err) => {
-  console.log("error? ", err)
-})
-
-camera.on('closed', (event) => {
-  console.log("closed? ", event)
-})
+}
